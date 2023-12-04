55cb44c228080478482c560dcd565aa538446870
Nursultan Sagynbek <nursultan1196@gmail.com>
2023-12-04 17:29:23 +0600

    add .env file

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,34 @@
-
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
-
-import Login from "./components/Login"
-import RootLayout from "./layouts/RootLayout"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Authentication from './components/Authentication';
 import WelcomeBack from "./components/WelcomBack"
-import SignUp from "./components/SignUp"
 import EmailSent from "./components/EmailSent"
-
-
 import Welcom from "./components/Welcom"
-import Authentication from "./components/Authentication"
 
 
 
-
-function App() {
-
+const App = () => {
+  useEffect(() => {
+    const apiUrl = import.meta.env.VITE_API_BASE_URL;
+   
+  }, []);
 
   return (
     <Router>
       <Routes>
-
-        <Route path="/" element={<RootLayout/>}>
-
-          <Route path="/" element={<Login/>}/>
-          <Route path="/signup" element={<SignUp/>}/>
-          <Route path="/auth" element={<Authentication/>}/>
-
-          <Route path="/welcomback" element={<WelcomeBack/>}/>      
-          <Route path="/email" element={<EmailSent/>}/>
-          <Route path="/welcom" element={<Welcom/>}/>
-
-
-
-
-        </Route>
-
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/auth" element={<Authentication />} />
+        <Route path="/welcomback" element={<WelcomeBack />} />
+        <Route path="/email" element={<EmailSent />} />
+        <Route path="/welcom" element={<Welcom />} />
       </Routes>
     </Router>
+  );
+};
 
-  )
-}
+export default App;
 
-export default App
+
